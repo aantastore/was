2fe6d89f361f1c08ef848dd0637a55499ed4cb97
surantasembiringkembarens <91650298+aantastore@users.noreply.github.com>
2021-12-15 08:04:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Kt69lMVe90WLBkCFcoYIyA']
 global.channelYT = ['https://youtube.com/channel/UCPhU6uA_3gUaEvyrFZFgJqg'] 
-global.owner = ['6283146208804', '628817649108', '6283146208804']// Put your number here
-global.mods = ['6283146208804', '6283146208804', '6283146208804'] // Want some help?
-global.partner = ['6283146208804', '6283146208804', '0'] // Want some help?
+global.owner = ['6281262411188', '6281262411188', '6281262411188']// Put your number here
+global.mods = ['6281262411188', '6281262411188', '6281262411188'] // Want some help?
+global.partner = ['6281262411188', '6281262411188', '0'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
